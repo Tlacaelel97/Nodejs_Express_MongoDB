8b22aeb198074495177565d56f07e10b240ae8e5
Tlacaelel <tlacafv@gmail.com>
2024-02-17 21:02:22 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+https://bluuweb.github.io/node/
+https://youtube.com/playlist?list=PLPl81lqbj-4IEnmCXEJeEXPepr8gWtsl6&si=EVn3QKH6RnKsmwlC
---
+++ app.js
@@ -0,0 +1,32 @@
+// Llamamos a express para configurar el server http
+const express = require("express");
+const app = express();
+const port = 3000;
+//Motor de plantillas
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views"); // Directorio donde se encuentran las vistas
+
+//__dirname es la ruta dinamica donde esta alojado el archivo
+app.use(express.static(__dirname + "/public"));
+
+// Mandamos la espuesta en la raiz
+app.get("/", (req, res) => {
+  res.render("index", { titulo: "Mi titulo Dinámico" });
+});
+
+app.get("/servicios", (req, res) => {
+  res.render("servicios", {
+    tituloServicios: "Este es un mensaje dinámico de Servicios",
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    titulo: "404",
+    descripcion: "Titulo del sitio web",
+  });
+});
+// Configuramos el puerto u la conexión
+app.listen(port, () => {
+  console.log("Servidor a su servicio en el puerto", port);
+});
